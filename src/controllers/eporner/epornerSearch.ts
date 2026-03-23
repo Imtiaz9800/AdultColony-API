d54ed79b3dfeeb2251ef4dbfa6f30f447a80863e
Imtiaz9800 <120592668+Imtiaz9800@users.noreply.github.com>
2026-03-23 23:30:39 +0530

    Refactor epornerSearch function and enhance logging

--- src/controllers/eporner/epornerSearch.ts
+++ src/controllers/eporner/epornerSearch.ts
@@ -1,51 +1,60 @@
-import axios from "axios";
-import https from "https";
+import { scrapeContent } from "../../services/scrapers/eporner/epornerSearchController";
+import c from "../../utils/options";
+import { logger } from "../../utils/logger";
+import { maybeError, spacer } from "../../utils/modifier";
 import { Request, Response } from "express";
 
-export const epornerSearch = async (req: Request, res: Response) => {
+export async function searchEporner(req: Request, res: Response) {
   try {
+    /**
+     * @api {get} /eporner/search Search eporner videos
+     * @apiName Search eporner
+     * @apiGroup eporner
+     * @apiDescription Search eporner videos
+     * @apiParam {String} query queryword to search
+     * @apiParam {Number} [page=0] Page number
+     *
+     * @apiSuccessExample {json} Success-Response:
+     *    HTTP/1.1 200 OK
+     *    HTTP/1.1 400 Bad Request
+     *
+     * @apiExample {curl} curl
+     * curl -i https://adultcolonyapi.site/eporner/search?query=milf
+     * curl -i https://adultcolonyapi.site/eporner/search?query=milf&page=2
+     *
+     * @apiExample {js} JS/TS
+     * import axios from "axios"
+     *
+     * axios.get("https://adultcolonyapi.site/eporner/search?query=milf")
+     * .then(res => console.log(res.data))
+     * .catch(err => console.error(err))
+     *
+     * @apiExample {python} Python
+     * import aiohttp
+     * async with aiohttp.ClientSession() as session:
+     *  async with session.get("https://adultcolonyapi.site/eporner/search?query=milf") as resp:
+     *    print(await resp.json())
+     */
+
     const query = req.query.query as string;
-    const page = (req.query.page as string) || "1";
+    const page = req.query.page || 1;
+    if (!query) throw Error("Parameter query is required");
+    if (isNaN(Number(page))) throw Error("Parameter page must be a number");
 
-    if (!query) {
-      return res.json({ success: true, data: [] });
-    }
+    const url = `${c.EPORNER}/search/${spacer(query)}/${page}/`;
+    console.log(url);
 
-    const agent = new https.Agent({ family: 4 });
-
-    const response = await axios.get(
-      "https://www.eporner.com/api/v2/video/search/",
-      {
-        params: {
-          query,
-          per_page: 10,
-          page,
-          thumbsize: "big",
-          format: "json"
-        },
-        headers: {
-          "User-Agent": "Mozilla/5.0",
-          "Referer": "https://www.eporner.com/",
-          "Accept": "application/json"
-        },
-        httpsAgent: agent,
-        timeout: 20000
-      }
-    );
-
-    const videos = response.data.videos || [];
-
-    return res.json({
-      success: true,
-      data: videos
+    const data = await scrapeContent(url);
+    logger.info({
+      path: req.path,
+      query: req.query,
+      method: req.method,
+      ip: req.ip,
+      useragent: req.get("User-Agent"),
     });
-
-  } catch (err: any) {
-    console.log("❌ SEARCH ERROR:", err.message);
-
-    return res.json({
-      success: true,
-      data: []
-    });
+    res.json(data);
+  } catch (err) {
+    const e = err as Error;
+    res.status(400).json(maybeError(false, e.message));
   }
-};
+}
